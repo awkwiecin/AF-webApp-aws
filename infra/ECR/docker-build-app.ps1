6079337c2637751832bf23a0383418e9e1cbe8a4
Andrzej Frydryszak <andrzej.frydryszak@itmagination.com>
2022-04-20 15:32:24 +0200

    correction on dockerfile

--- infra/ECR/docker-build-app.ps1
+++ infra/ECR/docker-build-app.ps1
@@ -1,4 +1,4 @@
 echo "Building docker image..."
 docker build -t af-ecr -f ./AF-webApp-aws/Dockerfile .
 echo "Running docker container..."
-Start-Job{docker run -p 8080:80 af-ecr}
+Start-Job{docker run -p 7001:80 af-ecr}
